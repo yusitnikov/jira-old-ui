86630ba8336bf5e068fd8a0d1b73405343fa21ef
yusitnikov <yu.sitnikov@synergetica.co>
2018-03-12 11:56:32 +0200

    Don't expand assignee drop-down

--- jira-old-ui.user.js
+++ jira-old-ui.user.js
@@ -2,7 +2,7 @@
 // @name        Jira Old UI
 // @description Expands Jira board quick filters like it was in the old UI
 // @author      yusitnikov
-// @version     1.0
+// @version     1.1
 // @updateURL   https://github.com/yusitnikov/jira-old-ui/raw/master/jira-old-ui.user.js
 // @match       https://*.atlassian.net/*
 // @run-at      document-end
@@ -10,15 +10,13 @@
 // ==/UserScript==
 
 setInterval(function() {
-    $('#ghx-quick-filters').children('ul').children('li').slice(1).each(function() {
-        var $li = $(this),
-            $div = $li.children('div');
-        $li.css('display', 'block');
-        $li.children('button').hide();
-        $div.attr('class', '');
-        $div.attr('style', '');
-        $div.find('li.ghx-filter-panel-dropdown').remove();
-        $div.find('li').css('display', 'inline');
-        $div.find('ul').css({ 'max-width': 'none', padding: 5 });
-    });
+    var $li = $('#ghx-quick-filters').children('ul').children('li').eq(1),
+        $div = $li.children('div');
+    $li.css('display', 'block');
+    $li.children('button').hide();
+    $div.attr('class', '');
+    $div.attr('style', '');
+    $div.find('li.ghx-filter-panel-dropdown').remove();
+    $div.find('li').css('display', 'inline');
+    $div.find('ul').css({ 'max-width': 'none', padding: 5 });
 }, 500);
